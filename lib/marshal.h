d5458fa15691b24c4cc106a70c1bc71122b9b7c4
Hermann He <myhechh@gmail.com>
2015-04-13 15:07:05 -0400

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit 2fcff908e9d709b8f6c546512c51d6435b91ddff, reversing
    changes made to 8eb0d6fc1266f028af88f117d7d35c8af1fe51c3.

--- lib/marshal.h
+++ lib/marshal.h
@@ -1,36 +1,17 @@
 #ifndef __MARSHAL_H__
-#define __MARSHAL_H__
 
-/* Defines for returned status */
-#define STORED          0
-#define NOT_STORED      1
-#define EXISTS          2
-#define NOT_FOUND       3
+struct operation;
 
-/* Support KV Store opperations */
-typedef enum {
-        GET,
-        SET
-};
-
-/* Operation struct used for gwkv opperations */
-struct operation {
-        method method_type;
-        const char* key;
-        size_t key_length;
-        const char* value;
-        size_t value_length;
-}
-
-int
+int 
 gwkv_marshal_server(struct operation* data, int status, char** ascii);
 
-int
-gwkv_marshal_client(struct operation* data, char** ascii);
 
-int
-gwkv_demarshal_server(char* ascii, struct** op);
+int 
+gwkv_marshal_client(structu operation* data, char** ascii);
 
-int
+int 
+gwkv_demarshal_server(char* ascii, struct** op, int* status);
+
+int 
 gwkv_demarshal_client(char* ascii, struct** op, int* status);
 #endif//__MARSHAL_H__
