9f575747cae0b8107ba551cb391979e6b15443ea
Phil Lopreiato <plnyyanks@gmail.com>
2015-04-14 13:16:26 -0400

    Make hash values always positive

--- server/gwkv_ht_wrapper.c
+++ server/gwkv_ht_wrapper.c
@@ -127,7 +127,9 @@ gwkv_server_free(struct gwkv_server* server)
 
 int
 gwkv_murmur_hash(char* key){
-	return murmurhash(key, strlen(key), 0);//takes in the key, the length of the key, and a seed
+	int hash = murmurhash(key, strlen(key), 0); //takes in the key, the length of the key, and a seed
+        if(hash < 0) hash *= -1; // make sure we only use positive numbers
+        return hash;
 }
 
 int
