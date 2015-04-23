26fafb29502b928e9a864f1bb6cf31a69c975155
Neel Shah <neel@shah7.com>
2015-04-23 16:39:48 -0400

    code cleanup -- removed unnecessary comments

--- server/server-kv.h
+++ server/server-kv.h
@@ -1,20 +1,5 @@
 #define MAX_CONCURRENCY 1000
 
-/*
-enum METHOD {
-        GET,
-        SET
-};*/
-
-/*
-struct operation {
-        enum METHOD method;
-        char *key;
-        int key_len;
-        char *value;
-        int value_len;
-};*/
-
 struct pool_list {
 	int fd;
 	struct pool_list *next;
