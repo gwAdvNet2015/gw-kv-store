5af8074fa86284659fd4681945c980b0ebcad3f1
Neel Shah <neel@shah7.com>
2015-04-23 16:18:51 -0400

    Removing method enum, operation struct definition, and return status macro defines since they have previously been moved to marshal.h

--- server/gwkv_ht_wrapper.h
+++ server/gwkv_ht_wrapper.h
@@ -18,27 +18,6 @@ struct gwkv_server {
         pthread_mutex_t lock;
 };
 
-/* Operations the table supports */
-typedef enum {
-        GET,
-        SET
-} method;
-
-/* Defines for result codes */
-#define STORED 0
-#define NOT_STORED 1
-#define EXISTS 2
-#define NOT_FOUND 3
-
-/* Datatype for wrapping datastore values */
-struct operation {
-        method method_type;
-        const char* key;
-        size_t key_length;
-        const char* value;
-        size_t value_length;
-};
-
 /* Defines for hashtable params */
 #define HT_SIZE 10 // number of buckets
 #define HT_BUCKET_LENGTH 10 // max entry/bucket (only if rebalancing)
